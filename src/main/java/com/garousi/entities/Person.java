2d8417dc207742951a77f1c840ba16a311838c88
Amir Hossein Garousi <amirgarousi73@gmail.com>
2024-09-21 19:23:12 +0330

    update person entity

--- src/main/java/com/garousi/entities/Person.java
+++ src/main/java/com/garousi/entities/Person.java
@@ -1,17 +1,20 @@
 package com.garousi.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "people")
 public class Person {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "person_id")
     private Integer id;
+
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
 
     public Integer getId() {
